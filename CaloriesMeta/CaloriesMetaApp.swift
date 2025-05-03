412b61f935fc51a691d34face4c93785b2c44583
olexey27 <allxyog@gmail.com>
2025-05-03 19:48:29 +0100

    Initial Commit

---
+++ CaloriesMeta/CaloriesMetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesMetaApp.swift
+//  CaloriesMeta
+//
+//  Created by Alexej K on 03/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesMetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
