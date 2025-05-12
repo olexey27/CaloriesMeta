9d1b71c4296be72063cd670b24613e11f2ade078
olexey27 <allxyog@gmail.com>
2025-05-12 04:36:05 +0100

    storage life cycle

--- CaloriesMeta/Model/FruitModel.swift
+++ CaloriesMeta/Model/FruitModel.swift
@@ -5,4 +5,16 @@
 //  Created by Alexej K on 12/05/2025.
 //
 
-import Foundation
+import SwiftUI
+
+// MARK: - FRUITS DATA MODEL
+
+struct Fruit: Identifiable {
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+    var gradientColors: [Color]
+    var description: String
+    var nutrition: [String]
+}
