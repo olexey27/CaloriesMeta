69addc6f2aa3e5e255a95eee7260e59e48bdd00b
olexey27 <allxyog@gmail.com>
2025-05-12 04:24:12 +0100

    create data model and fetch data

---
+++ CaloriesMeta/Data/FruitData.swift
@@ -0,0 +1,8 @@
+//
+//  FruitData.swift
+//  CaloriesMeta
+//
+//  Created by Alexej K on 12/05/2025.
+//
+
+import Foundation
---
+++ CaloriesMeta/Model/FruitModel.swift
@@ -0,0 +1,8 @@
+//
+//  FruitModel.swift
+//  CaloriesMeta
+//
+//  Created by Alexej K on 12/05/2025.
+//
+
+import Foundation
