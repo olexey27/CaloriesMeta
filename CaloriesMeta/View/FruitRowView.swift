412698fc1a18b327cf09153478d310082b7dd988
olexey27 <allxyog@gmail.com>
2025-05-12 05:12:27 +0100

    list view and retrieve data ising ForEach loop

---
+++ CaloriesMeta/View/FruitRowView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitRowView.swift
+//  CaloriesMeta
+//
+//  Created by Alexej K on 12/05/2025.
+//
+
+import SwiftUI
+
+struct FruitRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitRowView()
+}
