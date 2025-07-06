87ad30f2deb0fddf4d33e09c943db2afbf4f0902
olexey27 <allxyog@gmail.com>
2025-07-06 06:14:23 +0100

    SettingView

---
+++ CaloriesMeta/App/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  CaloriesMeta
+//
+//  Created by Alexej K on 16/05/2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
