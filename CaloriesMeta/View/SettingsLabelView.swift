73b23e1c3fc1c21cb09fb6730b481a6622df8735
olexey27 <allxyog@gmail.com>
2025-07-06 07:10:37 +0100

    settingsDone

---
+++ CaloriesMeta/View/SettingsLabelView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsLabelView.swift
+//  CaloriesMeta
+//
+//  Created by Alexej K on 06/07/2025.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsLabelView()
+}
